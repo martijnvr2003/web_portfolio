e611761e110f480ae5984b8f7c4d2dac5a0e525a
Martijn van Rooijen <534599@student.glu.nl>
2021-01-27 13:30:26 +0100

    frontpage

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>portfolio</title>
+    <link rel="stylesheet" href="assets/css/style.css">
+<body>
+    <div id="container" >
+        <header>
+            <nav>
+                <ul>
+                    <li class="active"><a href="#">Home</a></li>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Portfolio</a></li>
+                </ul>
+            </nav>
+        </header>
+        <section>
+            <h1 class="logo">Martijn <br> van Rooijen</h1>
+        </section>
+    </div>
+</body>
